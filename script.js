55db72011f2c76ad8d6e0b6681b25ce4bbb4bc5e
BrunoGuerrien <166255932+BrunoGuerrien@users.noreply.github.com>
2025-08-06 08:58:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,4 +12,24 @@ const perguntas = [
       enunciado: "Pergunta 2",
       alternativas: ["Alternativa 1", "Alternativa 2"],
     },
+    {
+      enunciado: "Pergunta 1",
+      alternativas: ["Alternativa 1", "Alternativa 2"],
+    },
+    {
+      enunciado: "Pergunta 1",
+      alternativas: ["Alternativa 1", "Alternativa 2"],
+    },
+    {
+      enunciado: "Pergunta 1",
+      alternativas: ["Alternativa 1", "Alternativa 2"],
+    }
   ];
+
+let atual = 0;
+let perguntaAtual;
+function mostraPergunta(){
+perguntaAtual = perguntas[atual];
+caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+mostraPergunta()
